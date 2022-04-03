dd47866cfced657213d0595e51cd9ce92ea69c7d
alexs <alexstlopes@gmail.com>
2022-04-03 03:40:57 -0300

    12. Conversão de imagens para escala de cinza

--- src/detection/TesteOpenCV.java
+++ src/detection/TesteOpenCV.java
@@ -2,6 +2,7 @@ package detection;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 import static org.opencv.imgcodecs.Imgcodecs.CV_LOAD_IMAGE_COLOR;
 import static org.opencv.imgcodecs.Imgcodecs.imread;
@@ -11,8 +12,13 @@ public class TesteOpenCV {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println(Core.VERSION);
 
-        Mat imagemColorida = imread("src/detection/opencv_java.jpg", CV_LOAD_IMAGE_COLOR);
+        Mat imagemColorida = imread("src/detection/opencv_java.jpg", CV_LOAD_IMAGE_COLOR);//Ler imagem colorida
         Utilities utilities = new Utilities();
         utilities.mostraImagem(utilities.convertMatToImage(imagemColorida));
+
+        Mat imagemCinza = new Mat();
+
+        Imgproc.cvtColor(imagemColorida, imagemCinza, Imgproc.COLOR_BGR2GRAY);//converte imagem de colorida para cinza
+        utilities.mostraImagem(utilities.convertMatToImage(imagemCinza));
     }
 }
